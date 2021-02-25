3032f3dc7baa8603c31b348343879f49c53bdc50
thisisishara <thisismaduishara@gmail.com>
2021-02-25 22:53:20 +0530

    DB Connection was Initialized. Item.java class was created. Items.jsp was created.

--- PAF_Practical_3/WebContent/Items.jsp
+++ PAF_Practical_3/WebContent/Items.jsp
@@ -1,5 +1,16 @@
+<%@ page import="com.paf3.model.*"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
+
+<%
+if (request.getParameter("itemCode") != null)
+ {
+	Item itemObj = new Item();
+	itemObj.connect();//For testing the connect method
+ }
+%>
+
+
 <!DOCTYPE html>
 <html>
 <head>
--- PAF_Practical_3/src/com/paf3/model/Item.java
+++ PAF_Practical_3/src/com/paf3/model/Item.java
@@ -2,5 +2,22 @@ package com.paf3.model;
 import java.sql.*;
 
 public class Item {
-	
+	public Connection connect()
+	{
+		Connection con = null;
+
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/PAF2021",	"root", "");
+			//For testing
+			System.out.print("Successfully connected");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return con;
+	}
 }
