09cff9ac45d3b2bfe51581678c315e93b385480e
thisisishara <thisismaduishara@gmail.com>
2021-02-26 00:00:56 +0530

    Updated web.xml to run Items.jsp when the project is Launched. Added Insert and Read Methods and Configured them to show results on jsp page.

--- PAF_Practical_3/WebContent/Items.jsp
+++ PAF_Practical_3/WebContent/Items.jsp
@@ -3,21 +3,41 @@
 	pageEncoding="ISO-8859-1"%>
 
 <%
-if (request.getParameter("itemCode") != null)
- {
+	//Insert item----------------------------------
+if (request.getParameter("itemCode") != null) {
 	Item itemObj = new Item();
-	itemObj.connect();//For testing the connect method
- }
+	String stsMsg = itemObj.insertItem(request.getParameter("itemCode"), request.getParameter("itemName"),
+	request.getParameter("itemPrice"), request.getParameter("itemDesc"));
+	session.setAttribute("statusMsg", stsMsg);
+}
 %>
-
 
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Items Management</title>
 </head>
 <body>
+
+	<h1>Items Management</h1>
+	<form method="post" action="Items.jsp">
+		Item code: <input name="itemCode" type="text"><br> Item
+		name: <input name="itemName" type="text"><br> Item price:
+		<input name="itemPrice" type="text"><br> Item
+		description: <input name="itemDesc" type="text"><br> <input
+			name="btnSubmit" type="submit" value="Save">
+	</form>
+	<%
+	if (session.getAttribute("statusMsg") != null) {
+		out.print(session.getAttribute("statusMsg"));
+	}
+	%>
+	<br>
+	<%
+		Item itemObj = new Item();
+	out.print(itemObj.readItems());
+	%>
 
 </body>
 </html>
--- PAF_Practical_3/src/com/paf3/model/Item.java
+++ PAF_Practical_3/src/com/paf3/model/Item.java
@@ -2,6 +2,8 @@ package com.paf3.model;
 import java.sql.*;
 
 public class Item {
+
+	//Connection
 	public Connection connect()
 	{
 		Connection con = null;
@@ -19,5 +21,100 @@ public class Item {
 		}
 
 		return con;
+	}
+
+	//Insert
+	public String insertItem(String code, String name, String price, String desc)
+	{
+		String output = "";
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{
+				return "Error while connecting to the database";
+			}
+
+			// create a prepared statement
+			String query = " insert into item(`itemID`,`itemCode`,`itemName`,`itemPrice`,`itemDesc`) values (?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, code);
+			preparedStmt.setString(3, name);
+			preparedStmt.setDouble(4, Double.parseDouble(price));
+			preparedStmt.setString(5, desc); 
+
+			//execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		}
+		catch (Exception e)
+		{
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	//Read
+	public String readItems()
+	{
+		String output = "";
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the HTML table to be displayed
+			output = "<table border='1'><tr><th>Item Code</th>"
+					+"<th>Item Name</th><th>Item Price</th>"
+					+ "<th>Item Description</th>"
+					+ "<th>Update</th><th>Remove</th></tr>";
+			
+			String query = "select * from item";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			
+			// iterate through the rows in the result set
+			while (rs.next())
+			{
+				String itemID = Integer.toString(rs.getInt("itemID"));
+				String itemCode = rs.getString("itemCode");
+				String itemName = rs.getString("itemName");
+				String itemPrice = Double.toString(rs.getDouble("itemPrice"));
+				String itemDesc = rs.getString("itemDesc");
+				
+				// Add a row into the HTML table
+				output += "<tr><td>" + itemCode + "</td>";
+				output += "<td>" + itemName + "</td>";
+				output += "<td>" + itemPrice + "</td>"; 
+				output += "<td>" + itemDesc + "</td>";
+				
+				// buttons
+				output += "<td><input name='btnUpdate' "
+						+ " type='button' value='Update'></td>"
+						+ "<td><form method='post' action='items.jsp'>"
+						+ "<input name='btnRemove' "
+						+ " type='submit' value='Remove'>"
+						+ "<input name='itemID' type='hidden' "
+						+ " value='" + itemID + "'>" + "</form></td></tr>";
+			}
+			
+			con.close();
+			
+			// Complete the HTML table
+			output += "</table>";
+		}
+		catch (Exception e)
+		{
+			output = "Error while reading the items.";
+			System.err.println(e.getMessage());
+		}
+		return output;
 	}
 }
